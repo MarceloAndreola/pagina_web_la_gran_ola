a32d536907c58f78215396b28fbd7f3c5460ce58
MarceloAndreola <marceloandreola18@gmail.com>
2025-01-27 15:11:21 -0300

    Mensaje descriptivo sobre los cambios realizados

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/pagina_web_la_gran_ola/' 
+    : '/',
   transpileDependencies: true
-})
+}
